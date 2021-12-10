b157d927116387a0e0aee7c54a3ace44f7e230eb
unknown <Administrator@jerryslab.local>
2021-12-10 01:40:57 -0500

    finished code

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import requests;
+import requests
 import json
+import keys
 
-api_url = "http://api.marketstack.com/v1/eod?access_key=de5ecfa193294a5f01c06f6d20dc01b1&symbols=AAPL&date_from=2021-12-09"
-api_key = "de5ecfa193294a5f01c06f6d20dc01b1"
+api_url = f'http://api.marketstack.com/v1/eod?access_key={keys.api_key}'
+print('hello andrew i will tell you the closing price of any stock by date')
+symbol = input('enter stock symbol: ')
+date = input('enter the date in YYYY-MM-DD format: ')
+api_url += f'&symbols={symbol}&date_from={date}'
 
 response = requests.get(api_url)
 data = response.json()
 closing_price = data['data'][0]['close']
-# def jprint(obj):
-#     text = json.dumps(obj, sort_keys=True, indent=4)
-#     print(text)
 
-print(f'Hey Andrew, appl is good as always, todays price is {closing_price}')
+# # def jprint(obj):
+# #     text = json.dumps(obj, sort_keys=True, indent=4)
+# #     print(text)
+
+print(f'Hey Andrew, {symbol} is good as always, the closing price on {date} is {closing_price}')
 
 
 
